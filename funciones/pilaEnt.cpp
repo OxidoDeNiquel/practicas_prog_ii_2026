26e8d721effe0d779f30dd58469356f27b122762
Niobe Claveria <niobeclaveria.val@gmail.com>
2026-03-09 09:40:06 +0000

    Avance p3

--- funciones/pilaEnt.cpp
+++ funciones/pilaEnt.cpp
@@ -12,12 +12,18 @@
 // Pre:  ---
 // Post: p = []
 void vaciar(PilaEnt &p){
-	p.datos=[];
+	p.cima=0;
 }
 
 // Pre:  p = [d_1, d_2, ..., d_K] AND K >= 0 AND K < DIM
 // Post: p = [d_1, d_2, ..., d_K, nuevo]
 void apilar(PilaEnt &p, const int nuevo){
-	p.cima++;
 	p.datos[p.cima]=nuevo;
+	p.cima++;
+}
+
+// Pre:  p = [d_1, d_2, ...,d_(K-1), d_K] AND K > 0
+// Post: p = [d_1, d_2, ..., d_(K-1)]
+void desapilar(PilaEnt &p){
+	p.cima--;
 }
